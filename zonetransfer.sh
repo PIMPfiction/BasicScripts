f35b4ef9f9ecab7657f96205e1084ebe1b496c7c
Goktug <26507081+PIMPfiction@users.noreply.github.com>
2017-08-09 19:41:53 +0300

    Gettin all Subdomains
    Getting all subdomains of target via dns zone transfer technique.Then scans these hosts with ip's of domains with Nmap TCP and UDP scan.

--- zonetransfer.sh
+++ zonetransfer.sh
@@ -7,20 +7,21 @@ read domain
 
 dig NS $domain | grep $domain. | awk -F" " '{ print $5 }' >> firstdomains
 
-#part2
+#part2(zone transfer section)
 
 cat $pwd/firstdomains | while read output 
 
 do dig @"$output" $domain axfr>> zoneoutput
 done
 
-#part3
+#part3(getting all sbudomains)
 onlydomain=$(echo $domain | awk -F"." {'print $1 '})
-
 
 cat zoneoutput | grep ".$domain" | awk -F" " '{print $1}' | grep $onlydomain | sort -u | uniq | grep -v "_" >> allsubs.txt
 
-#part4
+echo "You can interrupt the script, if you dont want to scan them."
+
+#part4(get ip for host)
 
 cat allsubs.txt | while read subdom
 
@@ -28,13 +29,11 @@ do ping -c 1 "$subdom" | awk  '/PING/{print $3 }' >> IPlist
 
 done
 
-#part5
-
+#part5(nmap Tcp Udp scan)
 
 cut -f 2 -d "(" IPlist | cut -f 1 -d ")" | sort -u | uniq >> NmapScan
 
 echo "You need to be Root for UDP scan " 
-
 
 cat NmapScan| while read nmapscan
 
