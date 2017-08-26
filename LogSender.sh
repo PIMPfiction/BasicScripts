6d16c23a50ad000c6373538c763a415b1bae2e56
Goktug <26507081+PIMPfiction@users.noreply.github.com>
2017-08-26 17:51:05 +0300

    LogSender.sh
    Log sender script for ELK stack and Splunk via netcat, you should configure your splunk to listen specific port, ELK stack has own log listener named logstash on port 5000.

---
+++ LogSender.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+echo -e " Address for send logs to"
+read address
+echo -e " Port of Log listener"
+read port
+echo -e "Log file or full directory"
+read logfile
+
+echo -e "Log Sending process has started" 
+
+nc -w 5 $address $port < $logfile
+
+cat $logfile >> ./sended"$logfile"
+
+echo " " > $logfile
+
+sleep 5
+
+if grep -Fxq "GET" $logfile
+then
+	echo "New requests are handling now,they will be sending soon."
+
+else
+	echo " No Request Yet" 
+	sleep 5
+fi
+
+
+
